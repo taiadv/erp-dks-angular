dadb70d50829f2321680c7a46c9ea80e52d88bf3
Taiane Costa <taianevilelamike@gmail.com>
2026-07-18 20:06:50 -0300

    dashborard

--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
