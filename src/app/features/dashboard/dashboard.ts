a05eb23e207103d26bf1d614075e017e43100051
Taiane Costa <taianevilelamike@gmail.com>
2026-07-20 21:52:20 -0300

    feat: dashboard com gráfico de vendas

--- src/app/features/dashboard/dashboard.ts
+++ src/app/features/dashboard/dashboard.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 
 import { DashboardCard } from './components/dashboard-card/dashboard-card';
+import { SalesChart } from './components/sales-chart/sales-chart';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [
-    DashboardCard
+    DashboardCard,
+    SalesChart
   ],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css'
