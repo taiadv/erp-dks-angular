d97c177111a8c931d9b27b9c8c43d5ab6e4dafbc
Taiane Costa <taianevilelamike@gmail.com>
2026-07-20 22:02:14 -0300

    feat: tabela de últimos pedidos

--- src/app/features/dashboard/dashboard.ts
+++ src/app/features/dashboard/dashboard.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 
 import { DashboardCard } from './components/dashboard-card/dashboard-card';
 import { SalesChart } from './components/sales-chart/sales-chart';
-
+import { RecentOrders } from './components/recent-orders/recent-orders';
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [
     DashboardCard,
-    SalesChart
+    SalesChart,
+    RecentOrders
   ],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css'
